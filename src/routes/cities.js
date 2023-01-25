175a05430b59050d8e9832825d36306d3c6df56c
Francesco Sansalvadore <f.sansalvadore@gmail.com>
2023-01-25 16:48:59 +0100

    Improve limit params

--- src/routes/cities.js
+++ src/routes/cities.js
@@ -7,22 +7,39 @@ const {
   getCompositeCities,
 } = require('../utils/helpers');
 
-// GET /cities
-// list of available cities
+// GET /cities?limit=3
 router.get('/', async (req, res) => {
+  const isNumber = new RegExp('^[0-9]$');
+  const limitDefault = 10;
   const limit = req.query.limit;
+  const isLimitValid = isNumber.test(limit);
 
-  const citiesRes = await Promise.all([
-    ...cities.map((city) => fetchCityWeatherData(city.lat, city.lon)),
-  ]);
-  const citiesWithBusinesses = await Promise.all(
-    citiesRes.map((city) => fetchCityBusinesses(city, limit))
+  // Retrieve weather data for all cities from openweather api
+  const citiesRes = await Promise.all(
+    cities.map((city) => fetchCityWeatherData(city.lat, city.lon))
   );
+  // Retrieve businesses data for all cities from yelp fusion api
+  const citiesWithBusinesses = await Promise.all(
+    citiesRes.map((city) =>
+      fetchCityBusinesses(city, isLimitValid ? limit : limitDefault)
+    )
+  );
+  // Combine data in one object by mathing coordinates
   const compositeCities = getCompositeCities(citiesRes, citiesWithBusinesses);
 
-  if (!citiesRes) res.status(500).send({ message: "Couln't find cities" });
+  // Send some kind of message if something doesn't work
+  if (!citiesRes)
+    res.status(500).send({ message: "Couln't find weather for cities" });
+  if (!citiesWithBusinesses)
+    res.status(500).send({ message: "Couln't find businesses for cities" });
 
-  res.status(200).send(compositeCities);
+  // Respond with composed object
+  res.status(200).send({
+    data: compositeCities,
+    message: !isLimitValid
+      ? `Limit param was incorrect and defaulted to ${limitDefault}`
+      : 'All good!',
+  });
 });
 
 module.exports = router;
